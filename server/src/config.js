5be34df75bf90e35b83c8f8eb7d3c10e6ff97847
NiicoBurgos <nicoburgos450@gmail.com>
2024-04-29 20:06:31 -0300

    readme modified

--- server/src/config.js
+++ server/src/config.js
@@ -3,6 +3,5 @@ export const DB_HOST = process.env.DB_HOST
 export const DB_USER = process.env.DB_USER
 export const DB_PASSWORD = process.env.DB_PASSWORD
 export const DB_DATABASE = process.env.DB_DATABASE
-export const DB_DATABASE_TEST = process.env.DB_DATABASE_TEST
 export const DB_PORT = process.env.DB_PORT || 5432
 export const DB_STRING = process.env.DB_STRING
